ef64acf291fdb2fc09b36d54802c28f740c94c59
Akhilesh-sahu2 <akhilsahu.pune@gmail.com>
2015-06-11 12:17:23 +0530

    comment

--- src/dataStructureAndAlgorithm/arrays/OneDArrays.java
+++ src/dataStructureAndAlgorithm/arrays/OneDArrays.java
@@ -5,7 +5,6 @@ public class OneDArrays {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hii");
-		System.out.println("Hii Algo"); 
+		System.out.println("Hii Algo");
 	}
-
 }
