6eeeddc431881960dd0b6a4804019a2294436e90
Akhilesh-sahu2 <akhilsahu.pune@gmail.com>
2015-06-11 11:53:46 +0530

    createing the ds and algo demo project

---
+++ src/dataStructureAndAlgorithm/arrays/OneDArrays.java
@@ -0,0 +1,11 @@
+package dataStructureAndAlgorithm.arrays;
+
+public class OneDArrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hii");
+		System.out.println("Hii Algo"); 
+	}
+
+}
