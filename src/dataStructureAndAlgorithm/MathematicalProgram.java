df798bafc031931edf196321bb74d65121fbf686
Akhilesh-sahu2 <akhilsahu.pune@gmail.com>
2015-06-15 16:28:42 +0530

    MathematicalProgramingClass added

--- src/dataStructureAndAlgorithm/MathematicalProgram.java
+++ src/dataStructureAndAlgorithm/MathematicalProgram.java
@@ -4,39 +4,47 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-import dataStructureAndAlgorithm.arrays.OneDArrays;
+import dataStructureAndAlgorithm.mathematicalProgram.OddNumberUptoRange;
+/**
+ * @author akhilesh_sahu
+ * MathematicalProgram to operate the mathematical function and operations 
+ */
+public class MathematicalProgram {
 
-public class Arrays {
-	public static void main(String[] args) throws IOException {
-		Arrays array = new Arrays();
-		OneDArrays oneD =  new OneDArrays();
+	public static void main(String[] args)throws IOException  {
+		OddNumberUptoRange oddNumberUptoRange= new 	OddNumberUptoRange();
+		MathematicalProgram mathematicalProgram = new MathematicalProgram();
 		int options = 0;
 		do {
+			
 			BufferedReader br = new BufferedReader(new InputStreamReader(
 					System.in));
-			array.printChoiceOptions();
+			mathematicalProgram.printChoiceOptions();
 			try {
 				options = Integer.parseInt(br.readLine());
 			} catch (NumberFormatException nfe) {
 				options = 0;
 			}
+			
 			switch (options) {
+			
 				case 1:
-					System.out.print("\tPlease enter number to Push\n");
+					System.out.print("\tPlease enter range to get odd number\n");
 					BufferedReader brOned = new BufferedReader(new InputStreamReader(System.in));
 					try {
-						oneD.push(Integer.parseInt(brOned.readLine()));
+						int oddNumberArray[] = oddNumberUptoRange.getOddNumberUptoRange(Integer.parseInt(brOned.readLine()));
+						mathematicalProgram.printOddNumber(oddNumberArray);
 					} catch (NumberFormatException nfe) {
 						System.out.print("\tPlease enter a valid number\n");
 					}
 					break;
 					
 				case 2:
-					System.out.print("\tPoped up number is "+oneD.pop()+"\n");
+				//	System.out.print("\tPoped up number is "+oneD.pop()+"\n");
 					break;
 					
 				case 3:
-					oneD.printElement();
+					//oneD.printElement();
 					break;
 					
 				default:
@@ -44,14 +52,24 @@ public class Arrays {
 		} while (options!=0);
 
 	}
-
+	
 	private void printChoiceOptions() {
 		System.out.print("\t============================================================\n");
 		System.out.print("\tEnter Your Choice using number\n");
-		System.out.print("\t1. One Dimension Array push operation\n");
+		System.out.print("\t1. Odd Numeber upto given range\n");
 		System.out.print("\t2. One Dimension Array pop operation\n");
 		System.out.print("\t3. One Dimension Array print operation\n");
 		System.out.print("\t============================================================\n");
 	}
 	
+	private void printOddNumber(int [] numberArray){
+		for (int count =0; count<numberArray.length; count++){
+			if(count==numberArray.length-1)
+				System.out.print(numberArray[count]+". \n");
+			else{
+				System.out.print(numberArray[count]+", ");
+			}
+		}
+	}
+
 }
